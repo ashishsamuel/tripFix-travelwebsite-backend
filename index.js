8688edc8c2ad91870e65222aae2c8a34fd607cea
ashish <ashishsamuelthomson@gmail.com>
2024-04-21 23:52:25 +0530

    first commit of tripfix backend server

---
+++ index.js
@@ -0,0 +1,12 @@
+//import dotenv ------ load .env contents into  process.env
+require('dotenv').config()
+
+// express used to create server
+const express = require('express')
+
+// import cors
+const cors = require('cors')
+
+// creating express server - creates an express application 
+const tripServer = express()
+
