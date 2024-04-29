fb84430c5acb1b18ed1142974e27af243e94af5d
ashish <ashishsamuelthomson@gmail.com>
2024-04-29 22:31:56 +0530

    index.js file changes

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ const cors = require('cors')
 // creating express server - creates an express application 
 const tripServer = express()
 
+//calling cors for using it for transfering data between application running in different ports
+tripServer.use(cors())
+// used for parsing json in express and it returns a middleware 
+tripServer.use(express.json())
+
+// customize port no to listen to http request
+// currently we use 4000 but after deplying the application to access the available port no we need to use process.env.PORT
+const PORT = 4000 || process.env.PORT
+
+// run server app 
+// listen method accepts the port no and the callback which will display the message that we want to display in the terminal 
+// while we run the server app 
+tripServer.listen(PORT,()=>{
+    console.log(`Trip Server started at port : ${PORT} and waiting for client requests`);
+})
+
+// to resolve http request that comes to the localhost url 
+// only get requests we will see in the browser
+tripServer.get()
